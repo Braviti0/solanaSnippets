dd5e8b61c7aea7a2ba8638a214b3ebb3689446d0
Agyeman <84294327+Braviti0@users.noreply.github.com>
2024-10-10 00:04:05 +0000

    Update connectToSolana.js

--- basicBlocks/connectToSolana.js
+++ basicBlocks/connectToSolana.js
@@ -1,1 +1,10 @@
-const
+const {
+  Connection,
+  clusterApiUrl
+} = require("@solana/web3.js");
+
+const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+
+console.log("connected successfully");
+
+
