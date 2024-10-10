6462daad1f6bc2ffdb232c659e759c7f3f0ff0ae
Agyeman <84294327+Braviti0@users.noreply.github.com>
2024-10-10 00:17:43 +0000

    Create getWalletBalance.js

---
+++ basicBlocks/getWalletBalance.js
@@ -0,0 +1,18 @@
+// import decimals support
+const {
+  LAMPORTS_PER_SOL
+} = require("@solana/web3.js");
+
+
+// getBalance function
+
+const getWalletBalance = async ( connection, publicKey ) => {
+  try {
+      const walletBalance = await connection.getBalance( publicKey );
+
+    console.log(`Wallet balance: ${parseInt(walletBalance) / LAMPORTS_PER_SOL} SOL `);
+  } catch (err) {
+      console.log(err);
+  }
+
+}
